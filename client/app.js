047d4b4b53523bb4919bcae482c44678ea1eeeb2
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-09-28 01:58:56 -0500

    Works except need to open/close Console first. Totally weird.

--- client/app.js
+++ client/app.js
@@ -1,3 +1,55 @@
 /**
  * Created by gfrethem on 9/27/15.
  */
+$(function() {
+    $.ajax({
+        url: '/getStudents'
+    }).done(function (response) {
+        for (i = 0; i < response.length; i++) {
+            var studentName = response[i].name;
+            var studentDescription = response[i].description;
+            var studentShoutOut = response[i].shoutOut;
+            var $htmlToAppend = '<li><h1>' + studentName + '</h1><p>' + studentDescription + '</p><p class="shoutOut">'
+                + studentShoutOut + '</p></li>';
+            $('.students').append($htmlToAppend);
+        }
+    });
+});
+
+
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel({wrap:'both'});
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
--- server/app.js
+++ server/app.js
@@ -1,3 +1,22 @@
 /**
  * Created by gfrethem on 9/27/15.
  */
+var express = require('express');
+var app = express();
+var students = require('./public/assets/data/students');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get('/getStudents', function(request, response) {
+    response.send(students);
+    console.log('Sent students.json');
+});
+
+var server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log('Listening on port: ', port);
+});
